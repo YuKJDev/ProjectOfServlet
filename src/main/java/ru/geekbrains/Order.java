e1aabec79e07e18571318b1577566f869a6414e1
YuKJDev <yury.a.kropotkin@gmail.com>
2020-08-06 00:16:45 +1100

    reload

--- src/main/java/ru/geekbrains/Order.java
+++ src/main/java/ru/geekbrains/Order.java
@@ -11,16 +11,14 @@ public class Order extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setContentType("text/html");
-
-        ru.geekbrains.Main.logger.info("New GET request");
+        Main.logger.info("New GET request");
         resp.getWriter().printf("<h1>This is a Order page</h1>");
 
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        ru.geekbrains.Main.logger.info("New POST request");
+        Main.logger.info("New POST request");
         resp.getWriter().printf("<h1>New POST request</h1>");
     }
 }
